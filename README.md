7b43f5c3e8280aa8a6fe042a9b56a705098a1d0d
huhn511 <sehe89@gmail.com>
2021-03-11 22:45:27 +0100

    add juniper_rocket_async

---
+++ README.md
@@ -0,0 +1,11 @@
+# NFT Backend
+The NFT Marketplace Backend Application
+
+
+## Development
+
+To run local:
+```bash
+cargo +nightly run
+```
+Visit http://localhost:8000/ in your browser.
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,70 @@
+#![feature(decl_macro, proc_macro_hygiene)]
+
 mod models;
 mod schemas;
 mod database;
 
-use models::human;
-use schemas::humans::{HumanSchema, Query, Mutation};
+// use models::human;
+// use schemas::humans::{HumanSchema, Query, Mutation};
 
-fn main() {
-    let hs = HumanSchema::new(Query, Mutation, juniper::EmptySubscription::new());
 
-    let context = schemas::humans::Context::new(database::DatabasePool{});
+use rocket::{response::content, State};
 
-    let (result, error) = juniper::execute_sync("query { apiVersion }", None, &hs, &juniper::Variables::new(), &context).unwrap();
+use juniper::{
+    tests::fixtures::starwars::schema::{Database, Query},
+    EmptyMutation, EmptySubscription, RootNode,
+};
 
-    println!("{:?}", result);
+type Schema = RootNode<'static, Query, EmptyMutation<Database>, EmptySubscription<Database>>;
+
+#[rocket::get("/")]
+fn graphiql() -> content::Html<String> {
+    juniper_rocket_async::graphiql_source("/graphql", None)
 }
+
+#[rocket::get("/graphql?<request>")]
+fn get_graphql_handler(
+    context: State<Database>,
+    request: juniper_rocket::GraphQLRequest,
+    schema: State<Schema>,
+) -> juniper_rocket_async::GraphQLResponse {
+    request.execute_sync(&schema, &context)
+}
+
+#[rocket::post("/graphql", data = "<request>")]
+fn post_graphql_handler(
+    context: State<Database>,
+    request: juniper_rocket::GraphQLRequest,
+    schema: State<Schema>,
+) -> juniper_rocket_async::GraphQLResponse {
+    request.execute_sync(&schema, &context)
+}
+
+
+#[rocket::main]
+
+async fn main() {
+    // let hs = HumanSchema::new(Query, Mutation, juniper::EmptySubscription::new());
+
+    // let context = schemas::humans::Context::new(database::DatabasePool{});
+
+    // let (result, error) = juniper::execute_sync("query { apiVersion }", None, &hs, &juniper::Variables::new(), &context).unwrap();
+
+    // println!("{:?}", result);
+
+    rocket::ignite()
+        .manage(Database::new())
+        .manage(Schema::new(
+            Query,
+            EmptyMutation::<Database>::new(),
+            EmptySubscription::<Database>::new(),
+        ))
+        .mount(
+            "/",
+            rocket::routes![graphiql, get_graphql_handler, post_graphql_handler],
+        )
+        .launch()
+        .await
+        .expect("server to launch");
+}
+
